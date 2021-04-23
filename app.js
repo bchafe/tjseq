85464e7e8a8fd281a53334c9058755a51ecd7403
Benjamin Chafe <bchafe@unb.ca>
2021-04-23 19:43:39 -0300

    Add kickEnv
    Change 'pad' to 'stepbutton in app.js, add kickEnv to app.js

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 
-
-kickTone = new Tone.Oscillator(30.87, "sine").toDestination()
+const kickEnv = new Tone.AmplitudeEnvelope({
+    attack: 0.1,
+    decay: 0.2,
+    sustain: 0.1,
+    release: 0.8
+}).toDestination()
+kickTone = new Tone.Oscillator(30.87, "sine").connect(kickEnv).start()
 
 
 
@@ -9,7 +14,7 @@ kickTone = new Tone.Oscillator(30.87, "sine").toDestination()
 const drumkit = document.querySelector('.drumkit');
 
 function playDrum(event){
-    if(event.target.classList.contains('pad')) {
+    if(event.target.classList.contains('stepbutton')) {
         event.preventDefault();
         let laneIndex = event.target.dataset.sound; // variable will have name of pad clicked (data-sound)
         kick(); //hardcoded for now
@@ -17,10 +22,11 @@ function playDrum(event){
 }
 
 function kick(){
-    kickTone.start();
+  /*  kickTone.start();
     setTimeout(function(){
         kickTone.stop()
-    }, 500);
+    }, 500);  */
+    kickEnv.triggerAttackRelease(0.5);
 }
 
 
